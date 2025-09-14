0cd31713a79c32c105d957a6000ea4333e682ad0
Mahmoud Mohamed <ma5975986@gmail.com>
2025-09-14 11:05:25 +0300

    Update quiz changing/navigating buttons with the current question index

--- lib/views/quiz_view/quiz_navigation_buttons.dart
+++ lib/views/quiz_view/quiz_navigation_buttons.dart
@@ -1,6 +1,6 @@
-import 'package:ai_interview_coach_app/ai/models/question_model.dart';
 import 'package:ai_interview_coach_app/views/quiz_view/custom_quiz_icon_button.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class QuizNavigationButtons extends StatelessWidget {
   const QuizNavigationButtons({
@@ -19,25 +19,28 @@ class QuizNavigationButtons extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      spacing: 8,
       children: [
         Opacity(
           opacity: currentIndex == 0 ? 0 : 1,
-          child: CustomQuizIconButton(
-            onPressed: () {
-              if (currentIndex > 0) {
-                pageController.animateToPage(
-                  currentIndex - 1,
-                  duration: const Duration(milliseconds: 400),
-                  curve: Curves.ease,
-                );
-              }
-            },
-            label: 'Previous',
-            icon: Icons.arrow_back,
-            btnColor: Theme.of(context).colorScheme.surface,
-            labelColor: Theme.of(context).colorScheme.secondary,
-            borderColor: Theme.of(context).colorScheme.outline,
-            iconColor: Theme.of(context).colorScheme.secondary,
+          child: Flexible(
+            child: CustomQuizIconButton(
+              onPressed: () {
+                if (currentIndex > 0) {
+                  pageController.animateToPage(
+                    currentIndex - 1,
+                    duration: const Duration(milliseconds: 400),
+                    curve: Curves.ease,
+                  );
+                }
+              },
+              label: 'Previous',
+              icon: Icons.arrow_back,
+              btnColor: Theme.of(context).colorScheme.surface,
+              labelColor: Theme.of(context).colorScheme.secondary,
+              borderColor: Theme.of(context).colorScheme.outline,
+              iconColor: Theme.of(context).colorScheme.secondary,
+            ),
           ),
         ),
         Text(
@@ -46,22 +49,26 @@ class QuizNavigationButtons extends StatelessWidget {
             color: Theme.of(context).colorScheme.secondary,
           ),
         ),
-        CustomQuizIconButton(
-          onPressed: () {
-            if (currentIndex < dummyQuestions.length) {
-              pageController.animateToPage(
-                currentIndex + 1,
-                duration: const Duration(milliseconds: 400),
-                curve: Curves.ease,
-              );
-            }
-          },
-          label: 'Next',
-          icon: Icons.arrow_forward,
-          btnColor: Theme.of(context).colorScheme.primary,
-          labelColor: Theme.of(context).colorScheme.onPrimary,
-          iconColor: Theme.of(context).colorScheme.onPrimary,
-          iconAlignment: IconAlignment.end,
+        Flexible(
+          child: CustomQuizIconButton(
+            onPressed: () {
+              if (currentIndex < totalQuestions) {
+                pageController.animateToPage(
+                  currentIndex + 1,
+                  duration: const Duration(milliseconds: 400),
+                  curve: Curves.ease,
+                );
+              }
+            },
+            label: currentIndex == totalQuestions - 1 ? 'Complete' : 'Next',
+            icon: currentIndex == totalQuestions - 1
+                ? FontAwesomeIcons.circleCheck
+                : Icons.arrow_forward,
+            btnColor: Theme.of(context).colorScheme.primary,
+            labelColor: Theme.of(context).colorScheme.onPrimary,
+            iconColor: Theme.of(context).colorScheme.onPrimary,
+            iconAlignment: IconAlignment.end,
+          ),
         ),
       ],
     );
