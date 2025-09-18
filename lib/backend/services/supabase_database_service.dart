881a50f18e9c5da80fde9b2f32d40244c1049f90
Mahmoud Mohamed <ma5975986@gmail.com>
2025-09-18 14:49:43 +0300

    Create supabase database service to handle user queries to database tables

---
+++ lib/backend/services/supabase_database_service.dart
@@ -0,0 +1,74 @@
+import 'package:ai_interview_coach_app/backend/models/performance_breackdown_model.dart';
+import 'package:ai_interview_coach_app/backend/models/quiz_session_model.dart';
+import 'package:ai_interview_coach_app/backend/models/suggestion_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseDatabaseService {
+  final _client = Supabase.instance.client;
+
+  /// For quiz_sessions table
+  Future<void> addQuizSession(QuizSessionModel model) async {
+    await _client.from('quiz_sessions').insert(model.toJson());
+  }
+
+  Future<List<QuizSessionModel>> getQuizSessions(String userId) async {
+    final result = await _client
+        .from('quiz_sessions')
+        .select()
+        .eq('user_id', userId);
+    final items = result
+        .map((item) => QuizSessionModel.fromJson(item))
+        .toList();
+    return items;
+  }
+
+  Future<QuizSessionModel> updateQuizSession(QuizSessionModel model) async {
+    final result = await _client
+        .from('quiz_sessions')
+        .update(model.toJson())
+        .eq('id', model.id!)
+        .select()
+        .single();
+    return QuizSessionModel.fromJson(result);
+  }
+
+  Future<void> deleteQuizSession(String quizId) async {
+    await _client.from('quiz_sessions').delete().eq('id', quizId);
+  }
+
+  /// For performance_breakdown table
+  Future<void> addPerformanceBreackdownItems(
+    List<PerformanceBreackdownModel> models,
+  ) async {
+    final items = models.map((item) => item.toJson());
+    await _client.from('performance_breakdown').insert(items);
+  }
+
+  Future<List<PerformanceBreackdownModel>> getPerformanceBreakdownItems(
+    String quizId,
+  ) async {
+    final result = await _client
+        .from('performance_breakdown')
+        .select()
+        .eq('quiz_id', quizId);
+    final items = result
+        .map((item) => PerformanceBreackdownModel.fromJson(item))
+        .toList();
+    return items;
+  }
+
+  /// For suggestions table
+  Future<void> addSuggestions(List<SuggestionModel> models) async {
+    final items = models.map((item) => item.toJson());
+    await _client.from('suggestions').insert(items);
+  }
+
+  Future<List<SuggestionModel>> getSuggestions(String quizId) async {
+    final result = await _client
+        .from('suggestions')
+        .select()
+        .eq('quiz_id', quizId);
+    final items = result.map((item) => SuggestionModel.fromJson(item)).toList();
+    return items;
+  }
+}
